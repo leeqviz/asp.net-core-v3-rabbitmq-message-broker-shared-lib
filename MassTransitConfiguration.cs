c1391790bdf1cf5fd0b4a609dff0a4686ad84b67
Leeqviz <leeqviz@gmail.com>
2021-09-16 15:39:45 +0300

    Add project files.

---
+++ MassTransitConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageBrokerShared
+{
+    public static class MassTransitConfiguration
+    {
+        public const string Uri = "rabbitmq://localhost";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string QueueName = "orders";
+    }
+}
---
+++ OrderFullInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MessageBrokerShared
+{
+    public class OrderFullInfo
+    {
+        public int Id { get; set; }
+        public string ClientId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
